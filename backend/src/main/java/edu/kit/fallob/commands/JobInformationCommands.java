0c032da9648cfe4bd728e94d9e3944bbee20afa6
Maik <maik.sept@gmx.de>
2022-07-11 02:28:45 +0200

    Created classes from package commands

---
+++ backend/src/main/java/edu/kit/fallob/commands/JobInformationCommands.java
@@ -0,0 +1,26 @@
+package commands;
+
+import java.util.List;
+
+import dataobjects.JobInformation;
+
+public class JobInformationCommands {
+	
+	
+	public JobInformation getSingleJobInformation(String username, int jobID) {
+		return null;
+	}
+	
+	public List<JobInformation> getMultipleJobInformation(String username, List<Integer> jobIDs) {
+		return null;
+	}
+	
+	public List<JobInformation> getAllJobInformation(String username) {
+		return null;
+	}
+	
+	public List<JobInformation> getAllGlobalJobInformation(String username) {
+		return null;
+	}
+
+}
---
+++ backend/src/main/java/edu/kit/fallob/commands/JobSubmitCommands.java
@@ -0,0 +1,25 @@
+package commands;
+
+import dataobjects.JobConfiguration;
+import dataobjects.JobDescription;
+
+public class JobSubmitCommands {
+	
+	
+	public int submitJobWithDescription(String username, JobDescription jobdescription, JobConfiguration jobConfiguration) {
+		return 0;
+	}
+	
+	public int submitJobWithDescriptionID(String username, int jobdescriptionID, JobConfiguration jobConfiguration) {
+		return 0;
+	}
+	
+	public int restartCanceledJob(String username, int jobID) {
+		return 0;
+	}
+	
+	public int saveJobDescription(String username, JobDescription jobdescription) {
+		return 0;
+	}
+
+}
