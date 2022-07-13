78c42aff3668047079d61b5d28cb2b6cfd8657b4
Captainval99 <valentin.schenk@gmx.de>
2022-07-13 19:23:14 +0200

    Implement all methods in DatabaseConnectionFactory class

--- backend/src/main/java/edu/kit/fallob/database/DatabaseConnectionFactory.java
+++ backend/src/main/java/edu/kit/fallob/database/DatabaseConnectionFactory.java
@@ -1,10 +1,42 @@
 package edu.kit.fallob.database;
 
-import java.sql.Connection;
+import edu.kit.fallob.configuration.FallobConfiguration;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Utility class that is responsible for establishing the connection to the database and creating Connection objects
+ * @author Valentin Schenk
+ * @version 1.0
+ */
 public final class DatabaseConnectionFactory {
 
-    public static Connection getConnection() {
-        return null;
+    /**
+     * private constructor to prevent instantiation of the class
+     */
+    private DatabaseConnectionFactory() {
+    }
+
+    //string that is used to get the database path into the right format for jdbc
+    private static final String DATABASE_PATH_FORMAT = "jdbc:h2:%s;IFEXISTS=TRUE";
+    //the username for the database
+    private static final String DATABASE_USER = "fallob";
+    //the password for the database
+    private static final String DATABASE_PASSWORD = "";
+
+
+    /**
+     * establishes the connection to the database and returns the Connection object
+     * @return the new Connection object
+     * @throws SQLException if the connection to the database couldn't be established
+     */
+    public static Connection getConnection() throws SQLException {
+        FallobConfiguration configuration = FallobConfiguration.getInstance();
+
+        String path = String.format(DATABASE_PATH_FORMAT, configuration.getDatabaseBasePath());
+
+        return DriverManager.getConnection(path, DATABASE_USER, DATABASE_PASSWORD);
     }
 }
