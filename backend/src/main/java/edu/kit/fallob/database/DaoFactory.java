4b1258d239dde2199e5c6bfef24e4361ec023f96
Captainval99 <valentin.schenk@gmx.de>
2022-07-16 18:21:00 +0200

    Implement all methods in DaoFactory

--- backend/src/main/java/edu/kit/fallob/database/DaoFactory.java
+++ backend/src/main/java/edu/kit/fallob/database/DaoFactory.java
@@ -1,5 +1,10 @@
 package edu.kit.fallob.database;
 
+/**
+ * class that is responsible for creating new DAO objects and returning them
+ * @author Valentin Schenk
+ * @version 1.0
+ */
 public class DaoFactory {
 
     private final UserDao userDao;
@@ -7,6 +12,10 @@ public class DaoFactory {
     private final EventDao eventDao;
     private final WarningDao warningDao;
 
+    /**
+     * constructor for the class
+     * creates the dao objects
+     */
     public DaoFactory() {
         this.userDao = new UserDaoImpl();
         this.jobDao = new JobDaoImpl();
@@ -14,19 +23,35 @@ public class DaoFactory {
         this.warningDao = new WarningDaoImpl();
     }
 
+    /**
+     * getter for the UserDao class
+     * @return the new UserDao instance
+     */
     public UserDao getUserDao() {
-        return null;
+        return this.userDao;
     }
 
+    /**
+     * getter for the JobDao class
+     * @return the new JobDao instance
+     */
     public JobDao getJobDao() {
-        return null;
+        return this.jobDao;
     }
 
+    /**
+     * getter for the EventDao class
+     * @return the new EventDao instance
+     */
     public EventDao getEventDao() {
-        return null;
+        return this.eventDao;
     }
 
+    /**
+     * getter for the WarningDao class
+     * @return the new WarningDao instance
+     */
     public WarningDao getWarningDao() {
-        return null;
+        return this.warningDao;
     }
 }
