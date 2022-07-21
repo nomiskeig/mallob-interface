50ee130a8436f492a7f40e5cc37a0608f1a7ce96
Simon Giek <nomiskeig@gmail.com>
2022-07-21 10:52:53 +0200

    Add controls to the visualization

--- frontend/src/pages/visualization/view/TimeLineComponent.jsx
+++ frontend/src/pages/visualization/view/TimeLineComponent.jsx
@@ -18,7 +18,6 @@ export class TimelineComponent extends React.Component {
 	}
 
 	render() {
-        console.log('updated')
 		function valueText(number) {
 			let text = format(
 				addMilliseconds(startTime, number),
@@ -32,8 +31,11 @@ export class TimelineComponent extends React.Component {
 			: new Date();
 		let timeDif = differenceInMilliseconds(currentTime, startTime);
 		if (!this.#changing) {
-            this.#position = differenceInMilliseconds(this.#timeManager.getNextTime(), startTime);
-		} 
+			this.#position = differenceInMilliseconds(
+				this.#timeManager.getNextTime(),
+				startTime
+			);
+		}
 
 		return (
 			<div className='container'>
@@ -46,17 +48,59 @@ export class TimelineComponent extends React.Component {
 					valueLabelFormat={valueText}
 					valueLabelDisplay='auto'
 					onChange={(event, newValue) => {
-						this.#position = newValue; 
+						this.#position = newValue;
 						this.#changing = true;
 					}}
 					onChangeCommitted={(event, newValue) => {
 						this.#changing = false;
 						this.#timeManager.setNextTime(addMilliseconds(startTime, newValue));
-                        console.log('set next time')
+						this.#timeManager.setJump();
 					}}
 				></Slider>
-				<div className='speedContainer'>
-					<input type='text' placeholder='1'></input>
+				<div className='speedContainer row'>
+					<button
+						className='startButton col'
+						onClick={() => this.#timeManager.setPaused(false)}
+                            disabled={!this.#timeManager.isPaused()}
+					>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='20'
+							height='20'
+							fill='currentColor'
+							className='bi bi-play-btn-fill'
+							viewBox='0 0 16 16'
+						>
+							<path d='M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.79-6.907A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z' />
+						</svg>
+					</button>
+					<button
+						className='pauseButton col'
+						onClick={() => this.#timeManager.setPaused(true)}
+                            disabled={this.#timeManager.isPaused()}
+					>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='20'
+							height='20'
+							fill='currentColor'
+							className='bi bi-pause-btn-fill'
+							viewBox='0 0 16 16'
+						>
+							<path d='M0 12V4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6.25-7C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z' />
+						</svg>
+					</button>
+					<form className='replaySpeedForm col'>
+						<label htmlFor='replaySpeed'>Replay speed</label>
+						<input
+							id='replaySpeed'
+							max={200}
+							min={-200}
+							type='number'
+							onChange={(e) => this.#timeManager.setMultiplier(e.target.value)}
+							value={this.#timeManager.getMultiplier()}
+						></input>
+					</form>
 				</div>
 			</div>
 		);
