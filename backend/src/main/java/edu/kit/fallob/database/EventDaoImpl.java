ef7e4e893fb2e1cb9de64f5ebd4e17f16bb618e9
Captainval99 <valentin.schenk@gmx.de>
2022-07-24 02:36:37 +0200

    Implement almost all methods in EventDaoImpl

--- backend/src/main/java/edu/kit/fallob/database/EventDaoImpl.java
+++ backend/src/main/java/edu/kit/fallob/database/EventDaoImpl.java
@@ -4,8 +4,10 @@ import edu.kit.fallob.mallobio.outputupdates.Event;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +19,9 @@ public class EventDaoImpl implements EventDao{
 
     private static final String INSERT_STATEMENT = "INSERT INTO event (jobId, rank, time, load, treeIndex)"
                                                     + " VALUES (?, ?, ?, ?, ?)";
+    private static final String REMOVE_BEFORE_TIME = "DELETE FROM event WHERE time < ?";
+    private static final String GET_BETWEEN_TIME = "SELECT * FROM event WHERE time > ? AND time <= ?";
+    private static final String TIME_OF_FIRST_EVENT = "SELECT time FROM event WHERE MIN(time)";
 
     private final Connection conn;
 
@@ -50,7 +55,14 @@ public class EventDaoImpl implements EventDao{
      */
     @Override
     public void removeEventsBeforeTime(LocalDateTime time) {
+        try {
+            PreparedStatement statement = this.conn.prepareStatement(REMOVE_BEFORE_TIME);
+            statement.setString(1, time.toString());
 
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -60,6 +72,7 @@ public class EventDaoImpl implements EventDao{
      */
     @Override
     public List<Event> getEventsByTime(LocalDateTime time) {
+        //TODO
         return null;
     }
 
@@ -71,7 +84,23 @@ public class EventDaoImpl implements EventDao{
      */
     @Override
     public List<Event> getEventsBetweenTime(LocalDateTime startTime, LocalDateTime endTime) {
-        return null;
+        List<Event> events = new ArrayList<>();
+        try {
+            PreparedStatement statement = this.conn.prepareStatement(GET_BETWEEN_TIME);
+            statement.setString(1, startTime.toString());
+            statement.setString(2, endTime.toString());
+
+            ResultSet result = statement.executeQuery();
+
+            while (result.next()) {
+                Event event = this.constructEvent(result);
+                events.add(event);
+            }
+
+            return events;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -80,6 +109,29 @@ public class EventDaoImpl implements EventDao{
      */
     @Override
     public LocalDateTime getTimeOfFirstEvent() {
+        try {
+            PreparedStatement statement = this.conn.prepareStatement(TIME_OF_FIRST_EVENT);
+
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                return result.getTimestamp(1).toLocalDateTime();
+            } else {
+                throw new RuntimeException();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * contructs a new Event object from the ResultSet from the database
+     * @param result the ResultSet that contains the information necessary to construct the event
+     * @return the constructed Event object
+     */
+    private Event constructEvent(ResultSet result) {
+        //TODO: missing constructor in Event class
         return null;
     }
 }
