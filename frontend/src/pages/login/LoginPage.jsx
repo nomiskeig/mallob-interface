3e5c55c351f1c3fb07b950b8f1de7d1d1d9c220a
Simon Wilhelm Schuebel <siwischbl@gmail.com>
2022-08-03 10:38:37 +0200

    Added inputs for username and password and onChange functions to track contents.

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -1,6 +1,8 @@
 import {useContext} from 'react';
 import {UserContext} from '../../context/UserContextProvider'
 import {useNavigate} from 'react-router-dom';
+import {Button} from '../../global/buttons/Button';
+import {InputLabel} from '../../global/textfields/Textfield';
 
 
 export function LoginPage(props) {
@@ -15,10 +17,42 @@ export function LoginPage(props) {
         userContext.login('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6InVzZXIifQ.lOw9NWomYJcsJrsLJdYbbS-14sEAe06WCKbkY3TbO6U');
         navigate('/jobs');
     }
-	return (
+
+
+    let btext = "Login";
+    const username = "username";
+    const password = "password";
+
+    let usernameContent = username;
+    let passwordContent = password;
+
+    const handleChangeUsername = event =>{
+        usernameContent = event.target.value;
+    }
+
+    const handleChangePassword = event =>{
+        passwordContent = event.target.value;
+    }
+
+
+    function login(){
+        alert("Logging in with : username=" + usernameContent + ", password=" + passwordContent);
+    }
+
+
+    return (
 		<div>
 			<button onClick={loginAdmin}>Log in as admin</button>
             <button onClick={loginUser}>Login in as user</button>
+
+
+            <div>
+                <InputLabel placeholder={username} id={username} onChange={handleChangeUsername}></InputLabel>
+                <InputLabel placeholder={password} id={password} onChange={handleChangePassword}></InputLabel>
+
+                <Button text={btext} onClick={login} />
+            </div>
+
 		</div>
 	);
 }
