61837c29156f1a88d681ea9c30b83e8f2d6c7751
Simon Giek <nomiskeig@gmail.com>
2022-08-17 16:36:18 +0200

    Format the login and register page

--- frontend/src/pages/login/LoginPage.jsx
+++ frontend/src/pages/login/LoginPage.jsx
@@ -1,99 +1,118 @@
-import {useContext, useState} from 'react';
-import {UserContext} from '../../context/UserContextProvider'
-import {useNavigate} from 'react-router-dom';
-import {Button} from '../../global/buttons/Button';
-import {InputLabel} from '../../global/textfields/Textfield';
+import { useContext, useState } from 'react';
+import {InfoContext} from '../../context/Info'
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../global/buttons/Button';
+import { InputLabel } from '../../global/textfields/Textfield';
 import { Link } from 'react-router-dom';
-import './LoginPage.scss'
+import './LoginPage.scss';
 
 export function LoginPage(props) {
-    let userContext = useContext(UserContext)
-    let navigate = useNavigate();
-
-    
-
-
-    const btext = "Login";
-    const username = "username";
-    const password = "password";
-
-    let usernameContent = username;
-    let passwordContent = password;
-
-
-    const handleChangeUsername = event =>{
-        usernameContent = event.target.value;
-    }
-
-    const handleChangePassword = event =>{
-        passwordContent = event.target.value;
-    }
-
-
-    const loginURL = process.env.REACT_APP_API_BASE_PATH + '/api/v1/users/login';
-    function login(){
-        //alert("Logging in with : username=" + usernameContent + ", password=" + passwordContent);
-        if (usernameContent === username || passwordContent === password){
-            alert("Please enter a valid username and/or password.");
-            return;
-        }
-
-        const axios = require('axios');
-        const response = axios.post(loginURL, {
-            username : {usernameContent},
-            password : {passwordContent}
-        });
-        
-        if (response.status === 200){ //request went through and username + password were accepted 
-            userContext.login(response.data);
-            navigate('/jobs');
-        } 
-
-        if (response.status === 404){ //user not found 
-            alert("Username not found.");
-        }
-        
-        else{
-            alert("Username or password wrong.");
-        }
-        //TODO : testing, error handling if pw was wrong, forward user onto job-page, tidy up login page (make it look nice )
-    }
-
-    function getInputLabel(placeholder, id, changeHandler, inputType){
-        return (
-            <div class="form-outline mb-3">
-                <InputLabel placeholder={placeholder} id={id} onChange={changeHandler} type={inputType} className="form-control form-control-lg"></InputLabel>
-            </div>
-        );
-    }
-
-    return (
-        <div className="py-5 loginPage">
-            <div>
-                <div class="text-center">
-                    <h1 class="mt-1 mb-5 pb-1 loginSlogan">Welcome to Mallob!</h1>
-                </div>
-                <div className="d-flex align-items-center justify-content-center container h-100 formContainer" id="logindiv">
-
-                    <form>
-
-                        {getInputLabel(username, username, handleChangeUsername, "text")}
-                        {getInputLabel(password, password, handleChangePassword, "password")}
-
-                        <div>
-                            <Button text={btext} onClick={login} className="btn btn-primary btn-lg btn-block" />
-                        </div>
-
-                        <div class="d-flex justify-content-around align-items-center mb-4">
-                            <Link to="/register" onClick={navigate('/register/')}>Register</Link>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-
 	let userContext = useContext(UserContext);
 	let navigate = useNavigate();
+
+	const btext = 'Login';
+	const username = 'username';
+	const password = 'password';
+
+	let usernameContent = username;
+	let passwordContent = password;
+
+	const handleChangeUsername = (event) => {
+		usernameContent = event.target.value;
+	};
+
+	const handleChangePassword = (event) => {
+		passwordContent = event.target.value;
+	};
+
+	const loginURL = process.env.REACT_APP_API_BASE_PATH + '/api/v1/users/login';
+	function login() {
+		//alert("Logging in with : username=" + usernameContent + ", password=" + passwordContent);
+		if (usernameContent === username || passwordContent === password) {
+			alert('Please enter a valid username and/or password.');
+			return;
+		}
+
+		const axios = require('axios');
+		const response = axios.post(loginURL, {
+			username: { usernameContent },
+			password: { passwordContent },
+		});
+
+		if (response.status === 200) {
+			//request went through and username + password were accepted
+			userContext.login(response.data);
+			navigate('/jobs');
+		}
+
+		if (response.status === 404) {
+			//user not found
+			alert('Username not found.');
+		} else {
+			alert('Username or password wrong.');
+		}
+		//TODO : testing, error handling if pw was wrong, forward user onto job-page, tidy up login page (make it look nice )
+	}
+
+	function getInputLabel(placeholder, id, changeHandler, inputType) {
+		return (
+			<div class='form-outline mb-3'>
+				<InputLabel
+					placeholder={placeholder}
+					id={id}
+					onChange={changeHandler}
+					type={inputType}
+					className='form-control form-control-lg'
+				></InputLabel>
+			</div>
+		);
+	}
+
+	return (
+		<div className='py-5 loginPage'>
+			<div>
+				<div class='text-center'>
+					<h1 class='mt-1 mb-5 pb-1 loginSlogan'>Welcome to Mallob!</h1>
+				</div>
+				<div
+					className='d-flex align-items-center justify-content-center container h-100 formContainer'
+					id='logindiv'
+				>
+					<form>
+						{getInputLabel(username, username, handleChangeUsername, 'text')}
+						{getInputLabel(
+							password,
+							password,
+							handleChangePassword,
+							'password'
+						)}
+
+						<div>
+							<Button
+								text={btext}
+								onClick={login}
+								className='btn btn-primary btn-lg btn-block'
+							/>
+						</div>
+
+						<div class='d-flex justify-content-around align-items-center mb-4'>
+							<Link to='/register' onClick={navigate('/register/')}>
+								Register
+							</Link>
+						</div>
+					</form>
+				</div>
+					<div>
+						<button onClick={loginAdmin}>Log in as admin</button>
+						<button onClick={loginUser}>Login in as user</button>
+						<button onClick={loginUnverifiedUser}>
+							Login as unverified user
+						</button>
+					</div>
+			</div>
+		</div>
+	);
+
 	function loginAdmin() {
 		userContext.login(
 			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIiwidmVyaWZpZWQiOiJ0cnVlIn0.9eR-rtIsjVmygEFa9CYKd9v3B5erW5lfK3kaENnNf9s'
@@ -107,19 +126,13 @@ export function LoginPage(props) {
 		navigate('/jobs');
 	}
 	function loginUnverifiedUser() {
-		userContext.login('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6InVzZXIiLCJ2ZXJpZmllZCI6ImZhbHNlIn0.Bq-zHXYMFc8-yGZL1iumHMiLV7Mm6aaxM4K1IZJQPj4'
+		userContext.login(
+			'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6InVzZXIiLCJ2ZXJpZmllZCI6ImZhbHNlIn0.Bq-zHXYMFc8-yGZL1iumHMiLV7Mm6aaxM4K1IZJQPj4'
 		);
 		navigate('/jobs');
 	}
-	return (
-		<div>
-			<button onClick={loginAdmin}>Log in as admin</button>
-			<button onClick={loginUser}>Login in as user</button>
-            <button onClick={loginUnverifiedUser}>Login as unverified user</button>
-		</div>
-	);
 
-    /**
+	/**
      * Approach like outlined in the first gui-sketches ;
      <div class="mb-3 row">
     <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
--- frontend/src/pages/register/RegisterPage.jsx
+++ frontend/src/pages/register/RegisterPage.jsx
@@ -1,96 +1,108 @@
-import {useContext} from 'react';
-import {UserContext} from '../../context/UserContextProvider'
-import {useNavigate} from 'react-router-dom';
-import {Button} from '../../global/buttons/Button';
-import {InputLabel} from '../../global/textfields/Textfield';
-import './../login/LoginPage.scss'
-
-
-
+import { useContext } from 'react';
+import { UserContext } from '../../context/UserContextProvider';
+import { useNavigate } from 'react-router-dom';
+import { Button } from '../../global/buttons/Button';
+import { InputLabel } from '../../global/textfields/Textfield';
+import './../login/LoginPage.scss';
 
 export function RegisterPage(props) {
-    let userContext = useContext(UserContext)
-    let navigate = useNavigate();
+	let userContext = useContext(UserContext);
+	let navigate = useNavigate();
 
-    let emailContent = "";
-    let usernameContent = "";
-    let passwordContent = "";
-    let confirmPasswordContent = "";
+	let emailContent = '';
+	let usernameContent = '';
+	let passwordContent = '';
+	let confirmPasswordContent = '';
 
-    const handleChangeEmail = event =>{
-      emailContent = event.target.value;
-  }
+	const handleChangeEmail = (event) => {
+		emailContent = event.target.value;
+	};
 
-  const handleChangeUsername = event =>{
-        usernameContent = event.target.value;
-  }
+	const handleChangeUsername = (event) => {
+		usernameContent = event.target.value;
+	};
 
-  const handleChangePassword = event =>{
-      passwordContent = event.target.value;
-  }
+	const handleChangePassword = (event) => {
+		passwordContent = event.target.value;
+	};
 
-  const handleChangeConfirmPassword = event =>{
-      confirmPasswordContent = event.target.value;
-  }
+	const handleChangeConfirmPassword = (event) => {
+		confirmPasswordContent = event.target.value;
+	};
 
-  
-  const registerURL = process.env.REACT_APP_API_BASE_PATH + '/api/v1/users/register';
-  function register(){
-      if (passwordContent !== confirmPasswordContent){
-            alert("Password does not match confirmation");
-      }
-      const axios = require('axios');
-        const response = axios.post(registerURL, {
-            "username" : {usernameContent},
-            "password" : {passwordContent},
-            "email" : {emailContent}
-        });
-        if (response.status === 200){ //register was successful
-            alert("Account created successfully.");
-            navigate('/jobs');
-        }
-        if (response.status !== 200){ 
-            alert("Username not available.");
-        }
-  }
+	const registerURL =
+		process.env.REACT_APP_API_BASE_PATH + '/api/v1/users/register';
+	function register() {
+		if (passwordContent !== confirmPasswordContent) {
+			alert('Password does not match confirmation');
+		}
+		const axios = require('axios');
+		const response = axios.post(registerURL, {
+			username: { usernameContent },
+			password: { passwordContent },
+			email: { emailContent },
+		});
+		if (response.status === 200) {
+			//register was successful
+			alert('Account created successfully.');
+			navigate('/jobs');
+		}
+		if (response.status !== 200) {
+			alert('Username not available.');
+		}
+	}
 
+	function getInputLabel(placeholder, changeHandler, inputType) {
+		return (
+			<div class='form-outline mb-4'>
+				<InputLabel
+					placeholder={placeholder}
+					onChange={changeHandler}
+					type={inputType}
+					className='form-control form-control-lg'
+				></InputLabel>
+			</div>
+		);
+	}
 
-  
-  function getInputLabel(placeholder,changeHandler, inputType){
-    return (
-        <div class="form-outline mb-4">
-        <InputLabel placeholder={placeholder} onChange={changeHandler} type={inputType} className="form-control form-control-lg"></InputLabel>
-        </div>
-    );
+	function goBackToLogin() {
+		navigate('/login/');
+	}
 
-}
-
-
-    function goBackToLogin(){
-      navigate('/login/');
-    }
-
-    return (
-        <div className="py-5 loginPage">
-            <div>
-                <div class="text-center">
-                    <h1 class="mt-1 mb-5 pb-1 loginSlogan">Sign up!</h1>
-                </div>
-                <div className="d-flex align-items-center justify-content-center container h-100 formContainer" id="logindiv">
-
-                    <form>
-
-                    {getInputLabel("youremail@bsp.exmpl", handleChangeEmail, "email")}
-                    {getInputLabel("username", handleChangeUsername, "text")}
-                    {getInputLabel("password", handleChangePassword, "password")}
-                    {getInputLabel("confirm password", handleChangeConfirmPassword, "password")}
-                <div>
-                    <Button text={"Register!"} onClick={register} className="btn btn-primary btn-lg btn-block" />
-                    <Button text={"Back to Login"} onClick={goBackToLogin} className="btn-link btn btn-primary btn-lg btn-block" />
-                </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+	return (
+		<div className='py-5 loginPage'>
+			<div>
+				<div class='text-center'>
+					<h1 class='mt-1 mb-5 pb-1 loginSlogan'>Sign up!</h1>
+				</div>
+				<div
+					className='d-flex align-items-center justify-content-center container h-100 formContainer'
+					id='logindiv'
+				>
+					<form>
+						{getInputLabel('youremail@bsp.exmpl', handleChangeEmail, 'email')}
+						{getInputLabel('username', handleChangeUsername, 'text')}
+						{getInputLabel('password', handleChangePassword, 'password')}
+						{getInputLabel(
+							'confirm password',
+							handleChangeConfirmPassword,
+							'password'
+						)}
+						<div>
+							<Button
+								text={'Register!'}
+								onClick={register}
+								className='btn btn-primary btn-lg btn-block'
+							/>
+							<Button
+								text={'Back to Login'}
+								onClick={goBackToLogin}
+								className='btn-link btn btn-primary btn-lg btn-block'
+							/>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 	);
 }
