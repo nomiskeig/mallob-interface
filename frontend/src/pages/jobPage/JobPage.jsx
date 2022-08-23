8e53f34876f381b5a8a36da65493e045336f3948
Simon Giek <nomiskeig@gmail.com>
2022-08-23 19:12:20 +0200

    Move the cancel job method to the jobcontext

--- frontend/src/pages/jobPage/JobPage.jsx
+++ frontend/src/pages/jobPage/JobPage.jsx
@@ -143,22 +143,10 @@ export function JobPage(props) {
 	}
 
 	function cancelJob() {
-		axios({
-			method: 'post',
-			url:
-				process.env.REACT_APP_API_BASE_PATH +
-				'/api/v1/jobs/cancel/single/' +
-				jobID,
-			headers: {
-				Authorization: 'Bearer ' + userContext.user.token,
-			},
-		}).then((res) => {
-			infoContext.handleInformation(
-				'Sucessesfully cancelled the job.',
-				TYPE_INFO
-			);
-		});
+        jobContext.cancelJob(jobID);
 	}
+
+
 
 	let parameterDisplayList = [];
 	if (job) {
