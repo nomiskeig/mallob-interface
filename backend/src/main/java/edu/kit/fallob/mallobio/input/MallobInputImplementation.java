dc26433140837bfe3ec290edd31e446820faf29e
Simon Wilhelm Schübel <siwischbl@gmail.com>
2022-09-07 10:45:05 +0200

    Improved diagnostics for mallob-input

--- backend/src/main/java/edu/kit/fallob/mallobio/input/MallobInputImplementation.java
+++ backend/src/main/java/edu/kit/fallob/mallobio/input/MallobInputImplementation.java
@@ -118,7 +118,7 @@ public class MallobInputImplementation implements MallobInput {
 				+ getFileName(NEW_JOB_FILENAME) + JSON_FILE_EXTENSION;
 
 		this.writeJsonInDirectory(json, absoluteFilePath);
-		System.out.println("Wrote Job-Json in directory : " + absoluteFilePath);
+		
 		return processID;
 	}
 	
@@ -134,6 +134,7 @@ public class MallobInputImplementation implements MallobInput {
 		FileWriter writer = new FileWriter(jsonFile.getAbsolutePath());
 		writer.write(json);
 		writer.close();
+		System.out.println("Wrote file in directory : " + path);
 	}
 	
 	private String getFileName(String fileBaseName) {
