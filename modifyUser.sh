4a73c27eb3cd9cfa58b8d1712b5ff2c2ce0ce91a
Simon Giek <nomiskeig@gmail.com>
2022-09-23 15:04:34 +0200

    Update the modify user script to allow modification of the users priority and set default configuration of user to 1

--- modifyUser.sh
+++ modifyUser.sh
@@ -4,20 +4,29 @@ then
     docker run fallob-dbaccess --help
     exit 0
 fi
-if [ $# -ne 2 ] && [ $# -ne 3 ]
+if [ $# -ne 2 ] && [ $# -ne 3 ] && [ $# -ne 4 ]
 then
     echo "Error, incorrect amount of arguments provided."
     exit 0
 fi
-if [ $# -eq 2 ]
+if [ $# -eq 4 ] && [ $1 != 'setPriority' ]
+then
+    echo "Error, incorrect amount of arguments provided."
+    exit 0
+fi
+if [ $# -eq 2 ] && [ $1 != 'setPriority' ]
 then 
     file=./fallob-data/fallob-configuration.json
 else
     file=$3
 fi
-empty=" "
-arguments="$1$empty$2$empty$file"
-echo $arguments;
+if [ $# -eq 4 ] && [ $1 == 'setPriority' ]
+then
+    file=$4
+fi
+if [ $# -eq 3 ] && [ $1 == 'setPriority' ]
+then file=./fallob-data/fallob-configuration.json
+fi
 databaseSource=$(grep -o '"databaseBasePath": "[^"]*' $file | grep -o '[^"]*$');
 databaseTarget=$databaseSource
 
@@ -32,7 +41,16 @@ then
     file=$(pwd)${file:1}
 fi
 
+if [ $1 != 'setPriority' ]
+then 
 docker run \
     --mount type=bind,source=$file,target=$file \
     --mount type=bind,source=$databaseSource,target=$databaseTarget \
     fallob-dbaccess $1 $2 $file
+else
+docker run \
+    --mount type=bind,source=$file,target=$file \
+    --mount type=bind,source=$databaseSource,target=$databaseTarget \
+    fallob-dbaccess $1 $2 $3 $file
+fi
+
